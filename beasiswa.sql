8bca2dc0eb6e93a513785f55dea32b37d69bd163
asista <asistaainun@gmail.com>
2023-11-06 20:27:50 +0700

    tambah sql

---
+++ beasiswa.sql
@@ -0,0 +1,91 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 06, 2023 at 02:20 PM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `beasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `beasiswa`
+--
+-- Error reading structure for table beasiswa.beasiswa: #1932 - Table &#039;beasiswa.beasiswa&#039; doesn&#039;t exist in engine
+-- Error reading data for table beasiswa.beasiswa: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `beasiswa`.`beasiswa`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `berkas`
+--
+-- Error reading structure for table beasiswa.berkas: #1932 - Table &#039;beasiswa.berkas&#039; doesn&#039;t exist in engine
+-- Error reading data for table beasiswa.berkas: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `beasiswa`.`berkas`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `city`
+--
+-- Error reading structure for table beasiswa.city: #1932 - Table &#039;beasiswa.city&#039; doesn&#039;t exist in engine
+-- Error reading data for table beasiswa.city: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `beasiswa`.`city`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patnership`
+--
+-- Error reading structure for table beasiswa.patnership: #1932 - Table &#039;beasiswa.patnership&#039; doesn&#039;t exist in engine
+-- Error reading data for table beasiswa.patnership: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `beasiswa`.`patnership`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pendaftaran`
+--
+-- Error reading structure for table beasiswa.pendaftaran: #1932 - Table &#039;beasiswa.pendaftaran&#039; doesn&#039;t exist in engine
+-- Error reading data for table beasiswa.pendaftaran: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `beasiswa`.`pendaftaran`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peserta`
+--
+-- Error reading structure for table beasiswa.peserta: #1932 - Table &#039;beasiswa.peserta&#039; doesn&#039;t exist in engine
+-- Error reading data for table beasiswa.peserta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `beasiswa`.`peserta`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `seleksi`
+--
+-- Error reading structure for table beasiswa.seleksi: #1932 - Table &#039;beasiswa.seleksi&#039; doesn&#039;t exist in engine
+-- Error reading data for table beasiswa.seleksi: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `beasiswa`.`seleksi`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tingkat`
+--
+-- Error reading structure for table beasiswa.tingkat: #1932 - Table &#039;beasiswa.tingkat&#039; doesn&#039;t exist in engine
+-- Error reading data for table beasiswa.tingkat: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `beasiswa`.`tingkat`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
